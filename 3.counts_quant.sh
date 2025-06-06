f1702a381f4d02b07087c56929e5d73115a34bf3
Francesco Edoardo Vallone <francescoedoardo.vallone@unito.it>
2025-06-06 15:30:28 +0200

    rename files by dir names

--- 3.counts_quant.sh
+++ 3.counts_quant.sh
@@ -6,7 +6,7 @@ THREADS=$(nproc --ignore=1)
 
 #defining paths
 PROJECT_DIR="/archive/home/frvallon/B-lymphoma"
-BAM_DIR="$PROJECT_DIR/alignment"
+BAM_PARENT="$PROJECT_DIR/bam"
 COUNTS_DIR="$PROJECT_DIR/raw_counts"
 GTF="$PROJECT_DIR/refseq/genomic.gtf"
 OUT_FILE="$COUNTS_DIR/gene_counts.txt"
@@ -14,7 +14,23 @@ OUT_FILE="$COUNTS_DIR/gene_counts.txt"
 #creating output directory
 mkdir -p "$COUNTS_DIR"
 
-#running featureCounts on all BAMs in one go
-featureCounts -p -T "$THREADS" -a "$GTF" -o "$OUT_FILE" "$BAM_DIR"/*.bam
+#creating temporary directory to store renamed BAMs
+TEMP_BAMS=$(mktemp -d)
 
-echo "FeatureCounts completed. Output saved to: $OUT_FILE"
+#copying and renaming each BAM file using its subdir name
+for DIR in "$BAM_PARENT"/*; do
+    SAMPLE=$(basename "$DIR")
+    BAM="$DIR/Aligned.sortedByCoord.out.bam"
+    if [[ -f "$BAM" ]]; then
+        cp "$BAM" "$TEMP_BAMS/${SAMPLE}.bam"
+    fi
+done
+
+#running featureCounts
+featureCounts -p -T "$THREADS" -t exon -g gene_id \
+    -a "$GTF" -o "$OUT_FILE" "$TEMP_BAMS"/*.bam
+
+#removing temp files
+rm -r "$TEMP_BAMS"
+
+echo "== FeatureCounts completed. Output saved to: $OUT_FILE =="
